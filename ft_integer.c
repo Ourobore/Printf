9a814c2196dcc735cd858c782db6c5c1908ca6ad
Loic CHAPRENET <lchapren@e2r7p9.42.fr>
2019-12-18 16:17:58 +0100

    Save

--- ft_integer.c
+++ ft_integer.c
@@ -6,7 +6,7 @@
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/18 08:53:21 by lchapren          #+#    #+#             */
-/*   Updated: 2019/12/18 11:45:33 by lchapren         ###   ########.fr       */
+/*   Updated: 2019/12/18 15:35:39 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,14 @@ void		integer_write_left(t_flags f, t_type t, int *i)
 	if (t.i < 0)
 	{
 		write(1, "-", 1);
-		if (t.i == -2147483648)
-			t.i = 2147483648;
-		else
-			t.i *= -1;
+		t.i *= -1;
+	}
+	if (t.i == 0)
+		*i += 1;
+	while (*i <= f.precision - len_int)
+	{
+		write(1, "0", 1);
+		*i += 1;
 	}
 	ft_putnbr_fd(t.i, 1);
 	while (*i < length - len_int)
@@ -33,7 +37,36 @@ void		integer_write_left(t_flags f, t_type t, int *i)
 		write(1, " ", 1);
 		*i += 1;
 	}
-	g_nb_carac = length;
+	g_nb_carac = *i + len_int;
+}
+
+void		integer_write_right(t_flags f, t_type t, int *i)
+{
+	int length;
+	int len_int;
+
+	len_int = get_length_int(t.i);
+	length = (f.width >= len_int) ? f.width : len_int;
+	while (*i < length - f.precision - 1)
+	{
+		write(1, " ", 1);
+		*i += 1;
+	}
+	if (t.i < 0)
+	{
+		write(1, "-", 1);
+		t.i *= -1;
+	}
+	if (t.i == 0)
+		*i += 1;
+	*i = 0;
+	while (*i <= f.precision - len_int)
+	{
+		write(1, "0", 1);
+		*i += 1;
+	}
+	ft_putnbr_fd(t.i, 1);
+	g_nb_carac = *i + length + len_int;
 }
 
 void		ft_integer(t_flags f, t_type t)
@@ -47,8 +80,9 @@ void		ft_integer(t_flags f, t_type t)
 	{
 		if (f.left)
 			integer_write_left(f, t, &i);
+		else
+			integer_write_right(f, t, &i);
 	}
 }
 
-//Precision == nb zero superpose et devant le nombre, si precision > width = agrandi
-//Si int < 0 print - tout de suite
+//Retour faux avec precision
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 08:09:05 by lchapren          #+#    #+#             */
-/*   Updated: 2019/12/18 11:46:01 by lchapren         ###   ########.fr       */
+/*   Updated: 2019/12/18 15:35:49 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int main()
 {
 	//t_flags f = ft_init_flags();
 	printf("==== REAL =====\n");
-	printf("|%-50ld|\n", -2147483648);
-	printf("%d\n", ft_printf("|%-50d|\n", -2147483648));
+	printf("|%2.5d|\n", 0);
+	printf("%d\n", ft_printf("|%2.5d|\n", 0));
 	//printf(":%d:\n", check_formulas("abc%-20c def %%%10c\n"));
 	printf("==== MINE =====\n");
 	//system("leaks a.out");
